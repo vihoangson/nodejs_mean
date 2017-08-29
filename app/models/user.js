6bf263f863f62e413b149093ef484ff427445be7
[Lampart] Vi Hoang Son <hoang_son@lampart-vn.com>
2017-08-29 09:05:45 +0700

    Finish lesson 2

--- app/models/user.js
+++ app/models/user.js
@@ -1,10 +1,28 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
 var Schema = mongoose.Schema;
 
+mongoose.Promise = global.Promise;
+
 var UserSchema = new Schema({
-	username: {type:String,lowercase:true,required:true,unique:true},
+	username: {type:String, lowercase:true, required: true,unique:true},
 	password: {type:String,required:true},
-	email: {type:String,lowercase:true,required:true,unique:true}
+	email:{type:String,required:true,lowercase:true,unique:true}
+});
+
+
+// `true` means this is a parallel middleware. You **must** specify `true`
+// as the second parameter if you want to use parallel middleware.
+if(false)
+UserSchema.pre('save', true, function(next, done) {
+
+ 	var user = this;
+ 	bcrypt.hash(user.password,null,null,function(err,hash){	 	
+		// if(err) return next(err);
+		// user.password = hash;
+		//next();
+ 	});
+  
 });
 
 module.exports = mongoose.model('User',UserSchema);
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var morgan = require('morgan');
 var User = require('./app/models/user');
 var bodyParser = require('body-parser');
 
+mongoose.Promise = global.Promise;
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(morgan('dev'));
@@ -24,16 +26,22 @@ else{
 
 
 
-app.post('/user',function(req,res){
-	var user = new User();
-
-
-	user.username = req.body.username;
-	user.password = req.body.password;
-	user.email = req.body.email;
-	user.save();
-	res.send('user created');
-
+app.post('/users',function(req,res){
+	if(req.body.username==null || req.body.username==""){
+		res.send('Ensure username were provided')
+	}else{		
+		var user = new User();
+		user.username = req.body.username;
+		user.password = req.body.password;
+		user.email = req.body.email;
+		user.save(function(err){
+			if(err){
+				res.send(err);
+			}else{
+				res.send('user created');
+			}
+		});	
+	}
 });
 
 
