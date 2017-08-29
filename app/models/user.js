43743b1bbe80b7d3322ceba9df4e2fa2142d1333
[Lampart] Vi Hoang Son <hoang_son@lampart-vn.com>
2017-08-29 10:29:45 +0700

    Finish lesson 3

--- app/models/user.js
+++ app/models/user.js
@@ -5,24 +5,24 @@ var Schema = mongoose.Schema;
 mongoose.Promise = global.Promise;
 
 var UserSchema = new Schema({
-	username: {type:String, lowercase:true, required: true,unique:true},
-	password: {type:String,required:true},
-	email:{type:String,required:true,lowercase:true,unique:true}
+    username: {type: String, lowercase: true, required: true, unique: true},
+    password: {type: String, required: true},
+    email: {type: String, required: true, lowercase: true, unique: true}
 });
 
 
 // `true` means this is a parallel middleware. You **must** specify `true`
 // as the second parameter if you want to use parallel middleware.
-if(false)
-UserSchema.pre('save', true, function(next, done) {
+if (false)
+    UserSchema.pre('save', true, function (next, done) {
 
- 	var user = this;
- 	bcrypt.hash(user.password,null,null,function(err,hash){	 	
-		// if(err) return next(err);
-		// user.password = hash;
-		//next();
- 	});
-  
-});
+        var user = this;
+        bcrypt.hash(user.password, null, null, function (err, hash) {
+            // if(err) return next(err);
+            // user.password = hash;
+            //next();
+        });
 
-module.exports = mongoose.model('User',UserSchema);
+    });
+
+module.exports = mongoose.model('User', UserSchema);
--- server.js
+++ server.js
@@ -1,50 +1,37 @@
 var express = require('express');
-var app = express ();
+var app = express();
 var port = 3000;
 var mongoose = require('mongoose');
 var morgan = require('morgan');
 var User = require('./app/models/user');
+var path = require('path');
 var bodyParser = require('body-parser');
+var routes = express.Router();
+var appRoutes = require('./app/routes/api')(routes);
 
 mongoose.Promise = global.Promise;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use('/api', appRoutes);
 
-mongoose.connect('mongodb://localhost:27017/tutorial',{
-	useMongoClient: true
-},function(err){
-	if(err){
-	console.log('Not connect to the database'+ err);
-}
-else{
-	console.log('Successfully connected to mongodb');
-}
+mongoose.connect('mongodb://localhost:27017/tutorial', {
+    useMongoClient: true
+}, function (err) {
+    if (err) {
+        console.log('Not connect to the database' + err);
+    }
+    else {
+        console.log('Successfully connected to mongodb');
+    }
 });
- 
 
-
-
-app.post('/users',function(req,res){
-	if(req.body.username==null || req.body.username==""){
-		res.send('Ensure username were provided')
-	}else{		
-		var user = new User();
-		user.username = req.body.username;
-		user.password = req.body.password;
-		user.email = req.body.email;
-		user.save(function(err){
-			if(err){
-				res.send(err);
-			}else{
-				res.send('user created');
-			}
-		});	
-	}
+app.get('*',function(req,res){
+    res.sendFile(path.join(__dirname+'/public/app/views/index.html'));
 });
 
 
-app.listen(port,function(){
-	console.log('Runing the server on port' + port)
+app.listen(port, function () {
+    console.log('Runing the server on port' + port)
 });
