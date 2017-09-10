7851c4f0d51c90b128efaee70eb5a5b4cf39f849
[Lampart] Vi Hoang Son <hoang_son@lampart-vn.com>
2017-09-10 15:38:43 +0700

    Lesson 8 - success get info by api

--- app/routes/api.js
+++ app/routes/api.js
@@ -13,39 +13,58 @@ module.exports = function (route) {
             user.email = req.body.email;
             user.save(function (err) {
                 if (err) {
-                    res.json({success:false,message: 'Username or Email already exits!'});
+                    res.json({success: false, message: 'Username or Email already exits!'});
                 } else {
-                    res.json({success:true,message: 'User created'});
+                    res.json({success: true, message: 'User created'});
                 }
             });
         }
     });
 
     route.post('/authenticate', function (req, res) {
-        if(req.body.username == null || req.body.username == ''){
-            res.json({success:false,message:'Don\'t have username'});
-        }else{
-            User.findOne({username: req.body.username}).select('email password username').exec(function(err,user){
-                if(err) throw err;
-                if(!user){
-                    res.json({success:false,message:'Error login'});
-                }else if(user){
+        if (req.body.username == null || req.body.username == '') {
+            res.json({success: false, message: 'Don\'t have username'});
+        } else {
+            User.findOne({username: req.body.username}).select('email password username').exec(function (err, user) {
+                if (err) throw err;
+                if (!user) {
+                    res.json({success: false, message: 'Error login'});
+                } else if (user) {
                     var validPassword = user.comparePassword(req.body.password);
 
-                    if(validPassword==true){
+                    if (validPassword == true) {
                         var token = jwt.sign({
                             username: req.body.username,
                             email: req.body.email
                         }, secret_string, {expiresIn: '2h'})
                         res.json({success: true, message: 'Success login', token: token});
 
-                    }else{
-                        res.json({success:false,message:'Error login'});
+                    } else {
+                        res.json({success: false, message: 'Error login'});
                     }
                 }
             });
         }
     });
 
+    route.use(function (req, res, next) {
+        var token = req.body.token || req.body.query || req.headers['x-access-token'];
+        if (token) {
+            jwt.verify(token, secret_string, function (err, decoded) {
+                if (err) {
+                    res.json({success: false, message: ' Token invalid'});
+                }else{
+                    req.decoded = decoded;
+                    next();
+                }
+            })
+        } else {
+            res.json({success: false, message: ' No token provider '})
+        }
+    });
+
+    route.post('/me', function (req, res) {
+        res.send(req.decoded);
+    });
     return route;
 }
