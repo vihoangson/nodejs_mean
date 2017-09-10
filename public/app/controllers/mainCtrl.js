b539b88079684409ecd3a28cc339c3214ab3306c
[Lampart] Vi Hoang Son <hoang_son@lampart-vn.com>
2017-09-10 16:07:33 +0700

    Lesson 8 - success get info by api from browser [Time: 00:37:23]

--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -1,30 +1,33 @@
 angular.module('mainControllers', ['authServices'])
-    .controller('mainCtrl', function ($http, $location, $timeout, Auth) {
+    .controller('mainCtrl', function ($http, $location, $timeout, Auth, $rootScope) {
         var app = this;
-        if(Auth.isLoggedIn()){
-            console.log('Success login');
-            Auth.getUser().then(function(data){
-                console.log(data);
-            })
-        }else{
-            console.log('Failure login');
-        }
+        $rootScope.$on('$routeChangeStart', function () {
+            if (Auth.isLoggedIn()) {
+                console.log('Success login');
+                Auth.getUser().then(function (data) {
+                    app.username = data.data.username
+                })
+            } else {
+                console.log('Failure login');
+            }
+        })
+
 
         this.dologin = function (loginData) {
 
             app.loading = true;
-            Auth.login(app.loginData).then(function(data){
+            Auth.login(app.loginData).then(function (data) {
                 app.loading = false;
-                if(data.data.success == true){
+                if (data.data.success == true) {
                     $location.path("/");
-                }else{
+                } else {
                     alert(data.data.message);
                 }
 
             })
         }
 
-        this.logout = function(){
+        this.logout = function () {
             Auth.logout();
         }
 
